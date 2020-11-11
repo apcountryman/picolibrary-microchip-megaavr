80c81e51df1c130ab03bac78cf3926126b3097d6
Andrew Countryman <apcountryman@gmail.com>
2020-11-10 16:25:54 -0800

    Simplify asynchronous serial transmitter implementation
    Supports #68.

--- include/picolibrary/microchip/avr/megaavr/asynchronous_serial.h
+++ include/picolibrary/microchip/avr/megaavr/asynchronous_serial.h
@@ -24,7 +24,7 @@
 
 #include <cstdint>
 
-#include "picolibrary/algorithm.h"
+#include "picolibrary/asynchronous_serial.h"
 #include "picolibrary/microchip/avr/megaavr/peripheral/usart.h"
 #include "picolibrary/result.h"
 #include "picolibrary/utility.h"
@@ -192,148 +192,10 @@ class Basic_Transmitter {
  * \tparam Data_Type The integral type used to hold the data to be transmitted.
  */
 template<typename Data_Type>
-class Transmitter {
+class Transmitter
+    : public ::picolibrary::Asynchronous_Serial::Transmitter<Basic_Transmitter<Data_Type>> {
   public:
-    /**
-     * \brief The integral type used to hold the data to be transmitted.
-     */
-    using Data = Data_Type;
-
-    /**
-     * \brief Constructor.
-     */
-    constexpr Transmitter() noexcept = default;
-
-    /**
-     * \brief Constructor.
-     *
-     * \param[in] usart The USART to be used by the transmitter.
-     * \param[in] data_bits The desired number of data bits.
-     * \param[in] parity The desired parity mode.
-     * \param[in] stop_bits The desired number of stop bits.
-     * \param[in] clock_configuration The desired clock generator configuration.
-     */
-    Transmitter(
-        Peripheral::USART &          usart,
-        Peripheral::USART::Data_Bits data_bits,
-        Peripheral::USART::Parity    parity,
-        Peripheral::USART::Stop_Bits stop_bits,
-        Clock_Configuration const &  clock_configuration ) noexcept :
-        m_usart{ &usart }
-    {
-        m_usart->disable();
-
-        m_usart->configure(
-            data_bits,
-            parity,
-            stop_bits,
-            clock_configuration.operating_speed,
-            clock_configuration.scaling_factor );
-    }
-
-    /**
-     * \brief Constructor.
-     *
-     * \param[in] source The source of the move.
-     */
-    constexpr Transmitter( Transmitter && source ) noexcept : m_usart{ source.m_usart }
-    {
-        source.m_usart = nullptr;
-    }
-
-    /**
-     * \todo #27
-     */
-    Transmitter( Transmitter const & ) = delete;
-
-    /**
-     * \brief Destructor.
-     */
-    ~Transmitter() noexcept
-    {
-        disable();
-    }
-
-    /**
-     * \brief Assignment operator.
-     *
-     * \param[in] expression The expression to be assigned.
-     *
-     * \return The assigned to object.
-     */
-    auto & operator=( Transmitter && expression ) noexcept
-    {
-        disable();
-
-        m_usart = expression.m_usart;
-
-        expression.m_usart = nullptr;
-
-        return *this;
-    }
-
-    /**
-     * \todo #27
-     *
-     * \return
-     */
-    auto operator=( Transmitter const & ) = delete;
-
-    /**
-     * \brief Initialize the transmitter's hardware.
-     *
-     * \return Success.
-     */
-    auto initialize() noexcept -> Result<Void, Void>
-    {
-        m_usart->enable_transmitter();
-
-        return {};
-    }
-
-    /**
-     * \brief Transmit data.
-     *
-     * \param[in] data The data to transmit.
-     *
-     * \return Success.
-     */
-    auto transmit( Data data ) noexcept -> Result<Void, Void>
-    {
-        while ( not m_usart->transmit_buffer_empty() ) {}
-
-        m_usart->transmit( data );
-
-        return {};
-    }
-
-    /**
-     * \brief Transmit a block of data.
-     *
-     * \param[in] begin The beginning of the block of data to transmit.
-     * \param[in] end The end of the block of data to transmit.
-     *
-     * \return Success.
-     */
-    auto transmit( Data const * begin, Data const * end ) noexcept
-    {
-        return for_each<Discard_Functor>(
-            begin, end, [this]( auto data ) noexcept { return transmit( data ); } );
-    }
-
-  private:
-    /**
-     * \brief The USART used by the transmitter.
-     */
-    Peripheral::USART * m_usart{};
-
-    /**
-     * \brief Disable the transmitter.
-     */
-    void disable() noexcept
-    {
-        if ( m_usart ) { m_usart->disable_transmitter(); }
-    }
+    using ::picolibrary::Asynchronous_Serial::Transmitter<Basic_Transmitter<Data_Type>>::Transmitter;
 };
 
 /**
