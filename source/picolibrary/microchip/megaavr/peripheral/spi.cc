fa4b758ffd634720c20f36b5de287aa3a776f3b8
Andrew Countryman <apcountryman@gmail.com>
2021-02-07 13:52:48 -0800

    Add register field bit position checks
    Supports #82.

--- source/picolibrary/microchip/megaavr/peripheral/spi.cc
+++ source/picolibrary/microchip/megaavr/peripheral/spi.cc
@@ -25,5 +25,7 @@
 namespace picolibrary::Microchip::megaAVR::Peripheral {
 
 static_assert( sizeof( SPI ) == 3 );
+static_assert( SPI::SPCR::Bit::SPIE == 7 );
+static_assert( SPI::SPSR::Bit::SPIF == 7 );
 
 } // namespace picolibrary::Microchip::megaAVR::Peripheral
