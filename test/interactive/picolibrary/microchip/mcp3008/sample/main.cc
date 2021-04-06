a002e7960cf076a2ef83380060a8a99d923896a1
Andrew Countryman <apcountryman@gmail.com>
2021-04-06 15:20:29 -0700

    Add Microchip MCP3008 sample interactive test program (#203)
    Resolves #188 (Add Microchip MCP3008 sample interactive test program).

---
+++ test/interactive/picolibrary/microchip/mcp3008/sample/main.cc
@@ -0,0 +1,82 @@
+/**
+ * picolibrary-microchip-megaavr
+ *
+ * Copyright 2020-2021, Andrew Countryman <apcountryman@gmail.com> and the
+ * picolibrary-microchip-megaavr contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/**
+ * \file
+ * \brief picolibrary::Microchip::MCP3008 sample interactive test program.
+ */
+
+#include <avr-libcpp/delay>
+
+#include "picolibrary/asynchronous_serial/stream.h"
+#include "picolibrary/gpio.h"
+#include "picolibrary/microchip/mcp3008.h"
+#include "picolibrary/microchip/megaavr/asynchronous_serial.h"
+#include "picolibrary/microchip/megaavr/gpio.h"
+#include "picolibrary/microchip/megaavr/peripheral.h"
+#include "picolibrary/microchip/megaavr/peripheral/spi.h"
+#include "picolibrary/microchip/megaavr/peripheral/usart.h"
+#include "picolibrary/microchip/megaavr/spi.h"
+#include "picolibrary/spi.h"
+#include "picolibrary/testing/interactive/microchip/mcp3008.h"
+
+namespace {
+
+using namespace ::picolibrary::Microchip::megaAVR::Peripheral;
+
+using ::picolibrary::Asynchronous_Serial::Unbuffered_Output_Stream;
+using ::picolibrary::GPIO::Active_Low_IO_Pin;
+using ::picolibrary::Microchip::MCP3008::Channel;
+using ::picolibrary::Microchip::MCP3008::Channel_Pair;
+using ::picolibrary::Microchip::MCP3008::Input;
+using ::picolibrary::Microchip::megaAVR::Asynchronous_Serial::Transmitter_8_N_1;
+using ::picolibrary::Microchip::megaAVR::GPIO::Push_Pull_IO_Pin;
+using ::picolibrary::Microchip::megaAVR::Peripheral::SPI;
+using ::picolibrary::Microchip::megaAVR::Peripheral::USART;
+using ::picolibrary::SPI::GPIO_Output_Pin_Device_Selector;
+using ::picolibrary::Testing::Interactive::Microchip::MCP3008::sample;
+
+using Controller = ::picolibrary::Microchip::megaAVR::SPI::Controller<SPI>;
+
+} // namespace
+
+/**
+ * \brief Execute the
+ *        picolibrary::Microchip::megaAVR::SPI::Controller<picolibrary::Microchip::megaAVR::Peripheral::SPI>
+ *        echo interactive test.
+ *
+ * \return 0.
+ */
+int main()
+{
+    sample<Unbuffered_Output_Stream>(
+        Transmitter_8_N_1{ TRANSMITTER_USART::instance(),
+                           { .operating_speed = USART::Operating_Speed::TRANSMITTER_CLOCK_GENERATOR_OPERATING_SPEED,
+                             .scaling_factor = TRANSMITTER_CLOCK_GENERATOR_SCALING_FACTOR } },
+        Controller{ Push_Pull_IO_Pin{ SCK_PORT::instance(), SCK_MASK },
+                    Push_Pull_IO_Pin{ MOSI_PORT::instance(), MOSI_MASK },
+                    CONTROLLER_SPI::instance() },
+        { .clock_rate     = SPI::Clock_Rate::CONTROLLER_CLOCK_RATE,
+          .clock_polarity = SPI::Clock_Polarity::IDLE_LOW,
+          .clock_phase    = SPI::Clock_Phase::CAPTURE_IDLE_TO_ACTIVE,
+          .bit_order      = SPI::Bit_Order::MSB_FIRST },
+        GPIO_Output_Pin_Device_Selector<Active_Low_IO_Pin<Push_Pull_IO_Pin>>{ SS_PORT::instance(), SS_MASK },
+        MCP3008_INPUT,
+        []() { avrlibcpp::delay_ms( 1000 ); } );
+
+    for ( ;; ) {}
+}
