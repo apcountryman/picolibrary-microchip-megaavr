c1975f18251f1ce5fcd8ea5164fdb56584c2f31d
Andrew Countryman <apcountryman@gmail.com>
2022-03-04 15:26:40 -0800

    Add TWI multiplexed signals lookup (#498)
    Resolves #487 (Add TWI multiplexed signals lookup).

--- include/picolibrary/microchip/megaavr/multiplexed_signals/atmega2560.h
+++ include/picolibrary/microchip/megaavr/multiplexed_signals/atmega2560.h
@@ -23,6 +23,7 @@
 #ifndef PICOLIBRARY_MICROCHIP_MEGAAVR_MULTIPLEXED_SIGNALS_ATMEGA2560_H
 #define PICOLIBRARY_MICROCHIP_MEGAAVR_MULTIPLEXED_SIGNALS_ATMEGA2560_H
 
+#include "picolibrary/microchip/megaavr/multiplexed_signals/atmega2560/twi.h"
 #include "picolibrary/microchip/megaavr/multiplexed_signals/atmega2560/usart.h"
 
 /**
