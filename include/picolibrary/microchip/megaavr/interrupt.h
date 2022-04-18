ef17148267dc112b5479e23f9b132a4d17fec7f3
Andrew Countryman <apcountryman@gmail.com>
2022-04-18 12:31:08 -0700

    Add Microchip megaAVR interrupt controller
    Resolves #617 (Add Microchip megaAVR interrupt controller).

--- include/picolibrary/microchip/megaavr/interrupt.h
+++ include/picolibrary/microchip/megaavr/interrupt.h
@@ -23,10 +23,111 @@
 #ifndef PICOLIBRARY_MICROCHIP_MEGAAVR_INTERRUPT_H
 #define PICOLIBRARY_MICROCHIP_MEGAAVR_INTERRUPT_H
 
+#include <avr-libcpp/interrupt>
+#include <cstdint>
+
 /**
  * \brief Microchip megaAVR interrupt facilities.
  */
 namespace picolibrary::Microchip::megaAVR::Interrupt {
+
+/**
+ * \brief Controller.
+ */
+class Controller {
+  public:
+    /**
+     * \brief Interrupts enable state.
+     */
+    using Interrupt_Enable_State = std::uint8_t;
+
+    /**
+     * \brief Constructor.
+     */
+    constexpr Controller() noexcept = default;
+
+    /**
+     * \brief Constructor.
+     *
+     * \param[in] source The source of the move.
+     */
+    constexpr Controller( Controller && source ) noexcept = default;
+
+    /**
+     * \brief Constructor.
+     *
+     * \param[in] original The original to copy.
+     */
+    constexpr Controller( Controller const & original ) noexcept = default;
+
+    /**
+     * \brief Destructor.
+     */
+    ~Controller() noexcept = default;
+
+    /**
+     * \brief Assignment operator.
+     *
+     * \param[in] expression The expression to be assigned.
+     *
+     * \return The assigned to object.
+     */
+    constexpr auto operator=( Controller && expression ) noexcept -> Controller & = default;
+
+    /**
+     * \brief Assignment operator.
+     *
+     * \param[in] expression The expression to be assigned.
+     *
+     * \return The assigned to object.
+     */
+    constexpr auto operator=( Controller const & expression ) noexcept -> Controller & = default;
+
+    /**
+     * \brief Disable interrupts.
+     */
+    void disable_interrupt() noexcept
+    {
+        cli();
+    }
+
+    /**
+     * \brief Enable interrupts.
+     */
+    void enable_interrupt() noexcept
+    {
+        sei();
+    }
+
+    /**
+     * \brief Save the current interrupts enable state.
+     *
+     * \return The current interrupts enable state.
+     */
+    auto save_interrupt_enable_state() const noexcept -> Interrupt_Enable_State
+    {
+        return *reinterpret_cast<std::uint8_t const volatile *>( SREG_ADDRESS );
+    }
+
+    /**
+     * \brief Restore the previously saved interrupts enable state.
+     *
+     * \param[in] interrupt_enable_state The previously saved interrupts enable state.
+     */
+    void restore_interrupt_enable_state( Interrupt_Enable_State interrupt_enable_state ) noexcept
+    {
+        *reinterpret_cast<std::uint8_t volatile *>( SREG_ADDRESS ) = interrupt_enable_state;
+
+        asm volatile( "" ::: "memory" );
+    }
+
+  private:
+    /**
+     * \brief SREG register address.
+     */
+    static constexpr auto SREG_ADDRESS = std::uintptr_t{ 0x005F };
+};
+
 } // namespace picolibrary::Microchip::megaAVR::Interrupt
 
 #endif // PICOLIBRARY_MICROCHIP_MEGAAVR_INTERRUPT_H
